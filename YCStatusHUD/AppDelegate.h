bd4f773ca219c79bd2716feb53157c13a76123e9
yaochao <yaochao@qq.com>
2015-01-26 18:11:38 +0000

    Initial Commit

---
+++ YCStatusHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YCStatusHUD
+//
+//  Created by PP on 15/1/26.
+//  Copyright (c) 2015年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YCStatusHUD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YCStatusHUD
+//
+//  Created by PP on 15/1/26.
+//  Copyright (c) 2015年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
